f4b26e094879bf7b6c9bf6e9486a7c7fc3b8fc4c
nero-15 <ti7805@gmail.com>
2021-02-28 13:03:27 +0900

    setting request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const api = require('./api/');
+const request = require('request');
 
 app.use(express.json());
 
@@ -14,6 +15,21 @@ app.use('/api', api);
 
 // routing
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/catImage/:width/:height', (req, res) => {
+	// 送受信の設定
+	const options = {
+		url: 'http://placekitten.com/' + `${req.params.width}/${req.params.height}`,
+		method: 'GET',
+		encoding: null
+	};
+
+	request(options, (err, response, body) => {
+		res.set('Content-Type', response.headers['content-type']);
+		res.send(body);
+	});
+});
+
 
 // error
 app.use((req, res) => {
